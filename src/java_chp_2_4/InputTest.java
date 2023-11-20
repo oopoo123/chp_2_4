bff710701455dd31772991f8fd791eeb5bef2486
oopoo123 <oopoo123@naver.com>
2023-11-20 14:21:59 +0900

    출력문 입력문

---
+++ src/java_chp_2_4/InputTest.java
@@ -0,0 +1,13 @@
+package java_chp_2_4;
+
+public class InputTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int keycode = System.in.read();
+		
+
+	}
+
+}
